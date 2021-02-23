a14aa0ba2c50376a365971f413c9986687128095
정회윤 <ghldbssla1997@gmil.com>
2021-02-23 23:19:15 +0900

    MyBatis 활용하기

---
+++ day09_jsp/WebContent/join_db.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%request.setCharacterEncoding("UTF-8"); %>
+<jsp:useBean id="udao" class="com.koreait.dao.UserDAO"/>
+<jsp:useBean id="udto" class="com.koreait.dto.UserDTO"/>
+<jsp:setProperty property="*" name="udto"/>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%
+		if(udao.join(udto)){
+			Cookie cookie = new Cookie("join_id",udto.getUserid());
+			response.addCookie(cookie);
+			response.sendRedirect("join_ok.jsp");
+		}else{
+	%>
+		<script>alert("잠시 후 다시 시도해 주세요.");</script>
+	<%
+			response.sendRedirect("joinview.jsp");
+		}
+	%>
+</body>
+</html>
---
+++ day09_jsp/src/com/koreait/dao/UserDAO.java
@@ -0,0 +1,50 @@
+package com.koreait.dao;
+
+import java.util.HashMap;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.koreait.dto.UserDTO;
+import com.koreait.mybatis.config.SqlMapConfig;
+
+//디비 접근을 위함
+public class UserDAO {
+	//만들었던 공장을 불러옴
+	SqlSessionFactory sqlfactory=SqlMapConfig.getInstance();
+	//생수병를 불러옴
+	SqlSession sqlsession;
+
+	public UserDAO() {
+		//true로 오토커밋(db에서 쿼리문 실행후 바로 저장됨)
+		sqlsession = sqlfactory.openSession(true);
+	}
+	public boolean join(UserDTO user) {
+		 //	HashMap<String, String> datas = new HashMap<>();
+		 //	datas.put("USERID", user.getUserid());
+		 //	datas.put("USERPW", user.getUserpw());
+		 //	datas.put("USERNAME", user.getUsername());
+		 //	datas.put("USERGENDER", user.getUsergender());
+		 //	datas.put("USERPHONE", user.getUserphone());
+		 //	datas.put("POSTCODE", user.getPostcode());
+		 //	datas.put("ADDR", user.getAddr());
+		 //	datas.put("DETAILADDR", user.getDetailaddr());
+		 //	datas.put("EXTRAADDR", user.getExtraaddr());
+		 //그냥 dto로 보내고 싶다!!
+		//user.xml에있는 User안의 join에 userdto객체를 넘겨줘라
+		//성공시 1을 리턴 => 성공한 행의 개수
+		//따라서 성공여부를 불린으로 처리한뒤 리턴!
+		return sqlsession.insert("User.join", user)==1;
+	}
+
+	public boolean login(String userid, String userpw) {
+		HashMap<String, String> datas = new HashMap<>();
+		datas.put("USERID", userid);
+		datas.put("USERPW", userpw);
+		//sqlsession.selectOne("User.login", datas)가 Object타입이기 때문에 다운캐스팅을 해준다.
+		//이미 오토박싱이 integer형으로 되고, Object로 업캐스팅이 된 상태이기 때문에
+		//integer타입으로 다운캐스팅이 되면 int형이랑 비교할때 알아서 오토 언박싱으로 int형이 된다
+		return (Integer)sqlsession.selectOne("User.login", datas)==1;
+		
+	}
+}
